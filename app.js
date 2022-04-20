a4cabe915994d23f8d25bf8ef5eff8ee26692f84
Ceyda <ceydaonemli2@gmail.com>
2022-04-20 12:33:48 +0300

    button functions created

--- app.js
+++ app.js
@@ -1,26 +1,45 @@
 new Vue({
-el:"#app",
-data:{
-player_heal:100,
-monster_heal:100,
-game_is_on:false,
-},
-
-methods:{
-    start_game:function(){
-    this.game_is_on=true; 
+    el: "#app",
+    data: {
+        player_heal: 100,
+        monster_heal: 100,
+        game_is_on: false,
     },
-    attack:function(){
 
-    },
-    special_attack:function(){
+    methods: {
+        start_game: function () {
+            this.game_is_on = true;
+        },
+        attack: function () {
+            var point = Math.ceil(Math.random() * 10);
+            this.monster_heal -= point;
+            this.monster_attack();
+            console.log(this.monster_heal)
+            console.log("Plyer", this.player_heal)
+        },
+        special_attack: function () {
+            var point = Math.ceil(Math.random() * 25);
+            this.monster_heal -= point;
+            this.monster_attack();
+            console.log(this.monster_heal)
+            console.log("Plyer", this.player_heal)
 
-    },
-    heal_up:function(){
+        },
+        heal_up: function () {
+            var point = Math.ceil(Math.random() * 20);
+            this.player_heal += point;
+            this.monster_attack();
+            console.log(this.monster_heal)
+            console.log("Plyer", this.player_heal)
 
-    },
-    give_up:function(){
-
+        },
+        give_up: function () {
+           this.player_heal=0;
+           console.log("p: ",this.player_heal)
+        },
+        monster_attack: function () {
+            var point = Math.ceil(Math.random() * 15);
+            this.player_heal -= point;
+        }
     }
-}
 })
